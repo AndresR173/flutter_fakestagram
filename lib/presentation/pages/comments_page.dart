f5de8da4e4fb614b4bd6834246180a53fefba425
Mike Ruiz <miguel.ruiz>
2022-10-31 01:26:24 -0500

    fixed comments page

--- lib/presentation/pages/comments_page.dart
+++ lib/presentation/pages/comments_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../models/comment.dart';
+import '../../utils/assets.dart';
 import '../widgets/comment_card.dart';
 import '../widgets/fakestagram_app_bar.dart';
 
@@ -30,41 +31,61 @@ class _CommentsPageState extends State<CommentsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: AppColors.primaryColor,
         appBar: FakestagramAppBar(
           title: 'Comments',
           hideButtons: true,
         ),
-        body: Column(
-          children: [
-            ListView.builder(
-              itemBuilder: (_, index) {
-                final comment = widget.comments[index];
-                return CommentCard(comment: comment);
-              },
-              itemCount: widget.comments.length,
-            ),
-            Row(
-              children: [
-                const CircleAvatar(
-                  radius: 16,
-                  child: Icon(Icons.person),
-                ),
-                const SizedBox(width: 10),
-                TextField(
-                  controller: _commentController,
-                  decoration: const InputDecoration(
-                    hintText: 'Add a comment...',
-                    hintStyle: TextStyle(color: Colors.white),
-                    border: InputBorder.none,
+        body: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: ListView.separated(
+                  separatorBuilder: (_, __) => const SizedBox(
+                    height: 5,
                   ),
+                  itemBuilder: (_, index) {
+                    final comment = widget.comments[index];
+                    return CommentCard(comment: comment);
+                  },
+                  itemCount: widget.comments.length,
                 ),
-                const SizedBox(width: 10),
-                IconButton(
-                    onPressed: () => widget.onNewComment(_commentController.text),
-                    icon: const Icon(Icons.send, color: Colors.white)),
-              ],
-            )
-          ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const CircleAvatar(
+                      radius: 16,
+                      child: Icon(Icons.person),
+                    ),
+                    const SizedBox(width: 10),
+                    SizedBox(
+                      width: 150,
+                      child: TextField(
+                        style: const TextStyle(color: Colors.white),
+                        controller: _commentController,
+                        decoration: const InputDecoration(
+                          hintText: 'Add a comment...',
+                          hintStyle: TextStyle(color: Colors.white),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    IconButton(
+                        onPressed: () => widget.onNewComment(_commentController.text),
+                        icon: const Icon(Icons.send, color: Colors.white)),
+                  ],
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
